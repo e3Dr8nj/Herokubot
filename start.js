4b3db20c394dc7642d1b2cdd5001a8cc5472798c
E H <c44.c4@yandex.ru>
2020-10-23 00:52:48 +0300

    'upd'

--- start.js
+++ start.js
@@ -1,5 +1,14 @@
 ﻿
 //__
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
