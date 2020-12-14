009a85228b74f8c57bb9d97ec5dc25c6c09ca4f4
E H <c44.c4@yandex.ru>
2020-12-14 22:12:33 +0300

    'upd'

--- modules/mute3.js
+++ modules/mute3.js
@@ -204,8 +204,11 @@ module.exports.commands.timemute={ on:true, aliase:'мут', run:async(client,me
               //return;
 //___upd 14.12
                if(client.muted[mmb.user.id]) {
+                        message.channel.send('Объект был замучен ранее, дождитесь пока адепт пустоты размутит его и замутит заново, это может занять где то от 0.001 секунды до вечности');
                         message.channel.send('^rewire \\размут '+mmb.toString());
-                        return message.channel.send('х99');
+                        await module.exports.delay(30*1000);
+                        
+                       // return;
 
                };
 //____
