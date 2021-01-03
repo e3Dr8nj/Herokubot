ab2ef2c485ac6b7a8683a27eeb17ccf8a4cd6b2f
E H <c44.c4@yandex.ru>
2021-01-03 22:07:00 +0300

    'upd'

--- modules/eval.js
+++ modules/eval.js
@@ -28,8 +28,8 @@ let process=async()=>{
 try {
       const code = args.slice(1).join(" ");
       console.log(code);
-      await message.delete().catch(err=>console.log(err));
-     await delay(1000); 
+     // await message.delete().catch(err=>console.log(err));
+     //await delay(1000); 
      // let lstMsg = await message.channel.fetchMessages();
       let evaled = await asyncEval(code);
       
