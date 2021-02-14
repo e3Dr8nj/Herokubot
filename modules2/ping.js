7dea9a1a5d553ade5e81b12f24e3b516a125d924
E H <c44.c4@yandex.ru>
2021-02-15 01:32:49 +0300

    'upd'

--- modules2/ping.js
+++ modules2/ping.js
@@ -99,8 +99,9 @@ module.exports.sub_ev01={ run:async(client,message)=>{try{
 
    if(message.channel.type=='dm' || message.author.bot||message.author==client.user) return;
   console.log('ddd');
-   //if(message.content.startsWith('+')) return;
+   
    console.log(message.content.startsWith('+'));
+   if(message.content.startsWith('+')) return;
    let nc = message.content.slice(1);
    
    let msg = await message.channel.send('@septapus comic '+nc);
