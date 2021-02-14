9545b5b4bb208c6a9d56d15f27e2f788b7bfd78b
E H <c44.c4@yandex.ru>
2021-02-15 02:04:05 +0300

    'upd'

--- modules2/ping.js
+++ modules2/ping.js
@@ -115,7 +115,7 @@ module.exports.sub_ev01={ run:async(client,message)=>{try{
 
 module.exports.events.message={ on:true,run:async(client,message)=>{try{
  //__
-     // module.exports.sub_ev01.run(client,message);
+      module.exports.sub_ev01.run(client,message);
   if(message.channel.type!='dm' ){
     if(message.content.startsWith('^rewire')&&message.author.bot){
       let str2 = message.content.trim().split(" ");
