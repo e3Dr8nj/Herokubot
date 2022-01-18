cea8cf963f38a0b09481e2b8759f0b8b8ed240c9
e3Dr8nj <c44.c4@yandex.ru>
2022-01-19 01:26:31 +0300

    'upd'

--- modules/chats.js
+++ modules/chats.js
@@ -1230,7 +1230,7 @@ exports.modeActivate=async(client,type,guild,owner_id,value)=>{try{ //
            await voice_chat.updateOverwrite(voice_chat.guild.roles.everyone, { SPEAK: bool }).catch(console.error);
         //
         voice_chat.members.map(m=>{
-       
+       await delay(1000)
           if(afk&&m&&m.id!=exports.voice_channels[voice_chat.id].owner_id) {
                 m.voice.setChannel(voice_chat.id).then(() => console.log(`Moved ${m.displayName}`)).catch(console.error);
            };
